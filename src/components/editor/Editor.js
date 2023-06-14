9fdcfde97987824653c269ae5975930f788dc454
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2023-06-14 19:52:17 -0400

    added text input

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,21 +1,19 @@
 import { useState, useRef } from "react"
+import { ActionTextArea } from "../ActionTextArea/ActionTextArea";
 
 
 export const Editor = () => {
     const [selectedText, setSelectedText] = useState("");
     const currentRef = useRef(null);
 
-    const handleClick = (stuff) => {
-        if(currentRef.current === stuff.target) {
-        const selection = stuff.target.value.substring(stuff.target.selectionStart, stuff.target.selectionEnd);
-        console.log(selection);
+    const selectionActiveHandler = (selection) => {
         setSelectedText(selection);
-        }
     }
-    return (<div>
-        <textarea id="editor" name="editor" rows="10" cols="80" ref={currentRef} onMouseUpCapture={(stuff)=>handleClick(stuff)}>
-        {/* probably have a bunch of text areas*/}
-        </textarea>
 
+    return (<div>
+        <ActionTextArea currentRef={currentRef} id={1} selectionActiveHandler={selectionActiveHandler}/>
+        <ActionTextArea currentRef={currentRef} id={2} selectionActiveHandler={selectionActiveHandler}/>
+        <ActionTextArea currentRef={currentRef} id={3} selectionActiveHandler={selectionActiveHandler}/>
+        <h1>{selectedText}</h1>
     </div>)
 }
